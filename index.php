01145ddbe9e851aa12a089c3591e21c0f5b63569
unknown <felipemrvieira@gmail.com>
2016-04-25 22:58:43 -0300

    setando a cor do status bar do android - tag meta

--- index.php
+++ index.php
@@ -61,6 +61,7 @@ include($dir.'/admin/logica/dao/conexao.php');
 <head>
     <title>Feirão Online de Imóveis</title>
   <meta charset="utf-8">
+    <meta name="theme-color" content="#2B669A">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
